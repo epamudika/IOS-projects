36cc29aa13dba02d2426e9dc45a97bd506e95eaa
Erandi <erandi201326@gmail.com>
2026-01-10 11:34:45 +0530

    Initial Commit

---
+++ ColorMatchingGame/ColorMatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchingGameApp.swift
+//  ColorMatchingGame
+//
+//  Created by COBSCCOMP242P-051 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
